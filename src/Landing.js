beb2e5e38bc684072ab74c7c60fc266a7b642763
iqbal choironi <iqbal.choironi@gmail.com>
2019-07-15 20:49:47 +0700

    add search in landing page

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Axios from "axios";
 
 import Paper from '@material-ui/core/Paper';
 import SearchIcon from '@material-ui/icons/Search';
@@ -8,12 +9,21 @@ import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton'
 import ButtonCard from './ButtonCard'
 
+import CardHerbMed from './CardHerbMed'
+import Card from './card'
+
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import { blue } from '@material-ui/core/colors';
+
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Person from '@material-ui/icons/Person';
+import CollectionsBookmark from '@material-ui/icons/CollectionsBookmark'
+import DateRange from '@material-ui/icons/DateRange'
 
 const BootstrapInput = withStyles(theme => ({
     root: {
@@ -68,52 +78,284 @@ const styles = {
     }
   };
 
-  function Landing(props) {
-    const { classes } = props;
-    const images = [{
-      url: '/asset/herbsmed.jpeg',
-      title: 'Herbal Medicine',
-      directory: '/herbmeds'
-    },
-    {
-      url: '/asset/plant.jpeg',
-      title: 'Plant',
-      directory: '/plant'
-    },
-    {
-      url: '/asset/compound.jpeg',
-      title: 'Compound',
-      directory: '/compound',
-    },
-    {
-      url: '/asset/comparision.jpeg',
-      title: 'Comparision',
-      directory: '/compare'
-    },
-    {
-      url: '/asset/prediction.jpeg',
-      title: 'Prediction',
-      directory: '/predict'
-    },
-    {
-      url: '/asset/tacit.jpeg',
-      title: 'Tacit Knowledge',
-      directory: '/tacit'
-    },
-    {
-      url: '/asset/explicit.jpeg',
-      title: 'Explicit Knowledge',
-      directory: '/explicit'
-    },
-    {
-      url: '/asset/plant_ethnic.jpeg',
-      title: 'Plant Used Ethnic',
-      directory: '/map/ethnic'
-    },]
-        return (
-            <div style={{
-                display:"flex",
-                flexDirection:"column"
+  function ListExplicit (props) {
+    return (
+        <div style={{
+            marginTop: "0",
+            marginBottom: "26px",
+            maxWidth: "80%"
+        }}> 
+        <Typography variant="subtitle1">
+        <Link style={{ 
+          textDecoration: 'none'
+          }} to={`/explicit/${ props.id }`}>
+            {props.title}
+        </Link>
+        </Typography>
+        <Typography variant="caption" >
+             <Person /> {props.name}
+        </Typography >
+        <Typography variant="caption" >
+             <CollectionsBookmark /> Conference paper <DateRange /> 12-12-2001
+        </Typography>
+        <p style={{
+          marginBlockStart: "5px",
+          fontFamily: "arial,sans-serif",
+          fontSize: "small"
+        }} className="block-with-text">
+            {props.abstract}
+        </p>
+       </div>
+    )
+}
+
+function ListTacit (props) {
+  return (
+      <div style={{
+          marginTop: "25px"
+      }}> 
+      <Typography variant="subtitle1" style={{
+          color: "#1976d8"
+      }}>
+      <Link to={`/tacit/${ props.id }`}>
+          {props.title}
+      </Link>
+      </Typography>
+      <Typography variant="caption" >
+           <Person /> {props.name}
+      </Typography >
+      <Typography variant="caption" >
+           <CollectionsBookmark /> Conference paper <DateRange /> 12-12-2001
+      </Typography>
+      <p className="block-with-text">
+          {props.abstract}
+      </p>
+     </div>
+  )
+}
+
+
+class Landing extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      onSearch: false,
+      inputSearch:'',
+      plan:[],
+      herbsmed:[],
+      tacit:[],
+      explicit:[]
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.getDataSearch = this.getDataSearch.bind(this);
+  }
+
+  handleInputChange(event) {
+    
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state.inputSearch)
+  }
+
+  async getDataSearch(){
+    this.setState({
+      loadData: true,
+      onSearch: true,
+    })
+    // const url = '/jamu/api/herbsmed/search';
+    // let axiosConfig = {
+    //   headers: {
+    //       'Content-Type': 'application/json'
+    //     }
+    //   };
+    // const res =  await Axios.get(url,{
+    //   params: {
+    //     search: this.state.inputSearch
+    //   }
+    // },axiosConfig);
+    // const { data } = await res;
+    // let newData = data.data;
+    // console.log(newData)
+    // this.setState({
+    //   onSearch: newData, 
+    //   loadData: false
+    // })
+
+    const urlSearchPlant = '/jamu/api/plant/search';
+    let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+    const resPlant =  await Axios.get(urlSearchPlant,{
+      params: {
+        search: this.state.inputSearch
+      }
+    },axiosConfig);
+    const dataPlant = await resPlant.data.data;
+    console.log(dataPlant)
+
+    const urlSearchHerbsMed = '/jamu/api/herbsmed/search';
+      const resHerbsMed  =  await Axios.get(urlSearchHerbsMed,{
+        params: {
+          search: this.state.inputSearch
+        }
+      },axiosConfig);
+      const dataHerbsMed = await resHerbsMed.data.data;
+      console.log(dataHerbsMed)
+
+      const UrlTacit = '/jamu/api/tacit/search/sort/';
+      const resTacit  =  await Axios.get(UrlTacit,{
+        params: {
+          search: this.state.inputSearch
+        }
+      },axiosConfig);
+      const dataTacit = await resTacit.data.data;
+      console.log(dataTacit)
+
+      const urlExplicit = '/jamu/api/explicit/search/sort/';
+      const resExplicit  =  await Axios.get(urlExplicit,{
+        params: {
+          search: this.state.inputSearch
+        }
+      },axiosConfig);
+      const dataExplicit = await resExplicit.data.data;
+      console.log(dataExplicit)
+
+      this.setState({
+        plan: dataPlant,
+        herbsmed: dataHerbsMed,
+        tacit: dataTacit,
+        explicit: dataExplicit
+      })
+  }
+
+    render (){
+      const { classes } = this.props;
+      const images = [{
+        url: '/asset/herbsmed.jpeg',
+        title: 'Herbal Medicine',
+        directory: '/herbmeds'
+      },
+      {
+        url: '/asset/plant.jpeg',
+        title: 'Plant',
+        directory: '/plant'
+      },
+      {
+        url: '/asset/compound.jpeg',
+        title: 'Compound',
+        directory: '/compound',
+      },
+      {
+        url: '/asset/comparision.jpeg',
+        title: 'Comparision',
+        directory: '/compare'
+      },
+      {
+        url: '/asset/prediction.jpeg',
+        title: 'Prediction',
+        directory: '/predict'
+      },
+      {
+        url: '/asset/tacit.jpeg',
+        title: 'Tacit Knowledge',
+        directory: '/tacit'
+      },
+      {
+        url: '/asset/explicit.jpeg',
+        title: 'Explicit Knowledge',
+        directory: '/explicit'
+      },
+      {
+        url: '/asset/plant_ethnic.jpeg',
+        title: 'Plant Used Ethnic',
+        directory: '/map/ethnic'
+      }]
+
+      return (
+        <div>
+          { this.state.onSearch ? 
+          <div style={{
+            display:"flex",
+            flexDirection:"column"
+            }}>
+             <div style={{
+                    marginTop: "60px",
+                    width: "100%",
+                    height:"150px",
+                    display: "flex",
+                    justifyContent : "center",
+                    alignItems : "center",
+                    backgroundColor: "#a9ba9d"
+                    
+                }}>
+                    <Paper className={classes.root} elevation={1}>
+                        <InputBase className={classes.input} name="inputSearch" value={this.state.inputSearch} onChange={this.handleInputChange} placeholder="Search here..." />
+                        <IconButton className={classes.iconButton} onClick={this.getDataSearch} aria-label="Search">
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
+                  
+                    
+                </div>
+
+                {
+                  this.state.herbsmed === [] ?
+                  null
+                  :
+                  <div className="for-card">
+                    {this.state.herbsmed.map(item =>
+                              <CardHerbMed key={item.idherbsmed} id={item.idherbsmed} name={item.name} efficacy={item.efficacy} reff={item.refCrude}/>
+                    )}
+                  </div>
+                }
+
+                {
+                  this.state.plan === [] ?
+                  null
+                  :
+                  <div className="for-card">
+                    {this.state.plan.map(item =>
+                              <Card key={item.id} name={item.sname} image={item.refimg} reff={item.refCrude} />
+                            )}
+                  </div>
+                }
+              
+                  <div style={{
+                    display:"flex",
+                    margin:"auto",
+                    // border:"hsl(0,0%,80%) 1px solid",
+                    width:"90%",
+                    marginBottom: "10px"
+                    }}>
+                      {this.state.explicit.map(item =>
+                        <ListExplicit key={item._id} id={item._id} name={item.firstName+' '+item.lastName} title={item.title} abstract={item.abstract} />
+                      )}
+                  </div>
+                  
+                  <div style={{
+                    display:"flex",
+                    margin:"auto",
+                    // border:"hsl(0,0%,80%) 1px solid",
+                    width:"90%",
+                    marginBottom: "10px"
+                    }}>
+                      {this.state.tacit.map(item =>
+                        <ListTacit key={item._id} id={item._id} name={item.firstName+' '+item.lastName} title={item.title} abstract={item.abstract} />
+                      )} 
+                    </div>
+          </div>
+          :
+          <div style={{
+            display:"flex",
+            flexDirection:"column"
             }}>
                 <div style={{
                     width: "100%",
@@ -129,7 +371,7 @@ const styles = {
                     {/* <InputLabel htmlFor="age-customized-native-simple" className={classes.bootstrapFormLabel}>
                         Age
                     </InputLabel> */}
-                    <NativeSelect
+                    {/* <NativeSelect
                         // value={this.state.age}
                         // onChange={this.handleChange}
                         input={<BootstrapInput name="age" id="age-customized-native-simple" />}
@@ -138,10 +380,10 @@ const styles = {
                         <option value={10}>Ten</option>
                         <option value={20}>Twenty</option>
                         <option value={30}>Thirty</option>
-                    </NativeSelect>
+                    </NativeSelect> */}
                     </FormControl>
-                        <InputBase className={classes.input} placeholder="Search here..." />
-                        <IconButton className={classes.iconButton} aria-label="Search">
+                        <InputBase className={classes.input} name="inputSearch" value={this.state.inputSearch} onChange={this.handleInputChange} placeholder="Search here..." />
+                        <IconButton className={classes.iconButton} onClick={this.getDataSearch} aria-label="Search">
                             <SearchIcon />
                         </IconButton>
                     </Paper>
@@ -186,13 +428,13 @@ const styles = {
                   </div>
                 </div> */}
                 <div style={{
-                   width:"95%",
-                   margin:"auto",
-                   marginTop: "20px",
-                   marginBottom: "20px",
-                   display: "grid",
-                   gridTemplateColumns: "repeat(auto-fill, minmax(16rem, 1fr))",
-                   gridGap: "2rem"
+                  width:"95%",
+                  margin:"auto",
+                  marginTop: "20px",
+                  marginBottom: "20px",
+                  display: "grid",
+                  gridTemplateColumns: "repeat(auto-fill, minmax(16rem, 1fr))",
+                  gridGap: "2rem"
                 }}>
                   {images.map(image => {
                     return <ButtonCard image={image} />
@@ -212,7 +454,11 @@ const styles = {
                 </div>
                 
             </div>
-        )
+          }
+        </div>
+      )
+    }
+        
     }
 
 Landing.propTypes = {
